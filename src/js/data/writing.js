8f72a8ecf60e501d83a147064cf5e878c16ec652
Ben Wiley <therealbenwiley@gmail.com>
2016-03-11 00:30:56 -0500

    More details added to writing section and web heading reworded.

--- src/js/data/writing.js
+++ src/js/data/writing.js
@@ -7,22 +7,25 @@ data.writing = {
       "items": [
         {
           "title": "Narrative Identity as Means for Understanding and Criticizing <i>The Two-Income Trap</i>",
+          "subtitle": "Critical analysis of Senator Elizabeth Warren and daughter Amelia Tyagi's acclaimed book.",
           "date": "11 May 2015",
           "link": "doc/narrative-identity-warren.pdf"
         },
         {
           "title": "Is Social Movement Organizing Easier in the Age of Social Media?",
+          "subtitle": "Discussion of new assets and difficulties movements face in the age of social media, using examples of the Zapatistas, Anonymous, the Arab Spring, Occupy, and Kony 2012.",
           "date": "15 Apr 2015",
           "link": "doc/movement-organizing-social-media.pdf"
         },
         {
           "title": "Erving Goffman and Ulrich Beck: Interaction Order or New World Order?",
+          "subtitle": "Examining untapped parallels between work of microsociologist Erving Goffman and macrosociologist Ulrich Beck, via themes of impression management, social construction, individualization, and social change.",
           "date": "12 Dec 2014",
           "link": "doc/goffman-beck.pdf"
         },
         {
           "title": "Can't They Just Get Along? (not quite)",
-          "subtitle": "Confronting and then sidestepping Marx and Foucault's irreconcilable differences in search for a sound theory of social change",
+          "subtitle": "Confronting and then sidestepping Marx and Foucault's irreconcilable differences in search for a sound theory of social change.",
           "date": "17 May 2013",
           "link": "doc/narrative-identity-warren.pdf"
         }
@@ -33,7 +36,7 @@ data.writing = {
       "items": [
         {
           "title": "Implementing an Ant Swarm Intelligence-Based Approach to Balancing Communication Network Loads",
-          "subtitle": "Investigating a decentralized network traffic routing algorithm based on behavior of ant colonies (with Tommy Rhodes).",
+          "subtitle": "Investigating a decentralized network traffic routing algorithm based on real-world behavior of ant colonies (with Tommy Rhodes).",
           "date": "6 May 2015",
           "link": "doc/ant-intelligence.pdf"
         }
@@ -44,6 +47,7 @@ data.writing = {
       "items": [
         {
           "title": "Indigenous Groups in Papua New Guinea Ready for War with ExxonMobil",
+          "subtitle": "Second-hand compliation of investigative reports into the oil giant's controversial Liquefied Natural Gas project.",
           "date": "14 May 2014",
           "link": "http://benwiley4000.kinja.com/indigenous-groups-in-papua-new-guinea-ready-for-war-wit-1576026036"
         }
