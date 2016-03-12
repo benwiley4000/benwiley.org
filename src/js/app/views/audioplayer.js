a4c633c37e55fe080cc1a29e2979dcb21d5cc5f1
Ben Wiley <therealbenwiley@gmail.com>
2016-03-12 10:11:32 -0500

    Audio seeking added to player.

--- src/js/app/views/audioplayer.js
+++ src/js/app/views/audioplayer.js
@@ -7,20 +7,32 @@ app.AudioPlayerView = Backbone.View.extend({
 
   events: {
     'click #play_pause_button': 'togglePause',
-    'click #skip_button': 'skipToNextTrack'
+    'click #skip_button': 'skipToNextTrack',
+    'touchstart #audio_progress_container': 'adjustProgress',
+    'touchmove #audio_progress_container': 'adjustProgress',
+    'mousedown #audio_progress_container': 'adjustProgress',
+    'mousemove #audio_progress_container': 'adjustProgress'
   },
 
   currentTrackIndex: 0,
 
   paused: true,
 
+  currentTargetTime: null,
+
+  seekInProgress: false,
+
   initialize: function () {
     this.data = data.audio;
 
     this.$playPauseButton = this.$('#play_pause_button');
+    this.$audioProgressContainer = this.$('#audio_progress_container');
     this.$progressBar = this.$('#audio_progress');
     this.$timeProgress = this.$('#audio_time_progress');
     this.$infoBox = this.$('#audio_info');
+
+    window.addEventListener('mouseup', this.seek.bind(this));
+    document.addEventListener('touchend', this.seek.bind(this));
 
     var audio = this.audio = $('<audio preload="metadata"><source/></audio>').get(0);
     audio.addEventListener('ended', this.skipToNextTrack.bind(this));
@@ -81,8 +93,11 @@ app.AudioPlayerView = Backbone.View.extend({
   },
 
   renderProgressBar: function () {
-    var currentTime = this.audio.currentTime;
-    var duration = this.audio.duration;
+    if (this.seekInProgress) {
+      return;
+    }
+    var currentTime = this.audio.currentTime || 0;
+    var duration = this.audio.duration || 0;
     this.$progressBar.width((100 * currentTime / duration) + '%');
     this.renderTimeProgress();
   },
@@ -97,6 +112,48 @@ app.AudioPlayerView = Backbone.View.extend({
     var title = 'Ben Wiley - ' + this.getCurrentTrackTitle();
     this.$el.attr('title', title);
     this.$infoBox.text(title);
+  },
+
+  adjustProgress: function (e) {
+    var evt = e.originalEvent;
+    var $progressContainer = this.$audioProgressContainer;
+    // make sure we don't select stuff in the background while seeking
+    if (evt.type === 'mousedown' || evt.type === 'touchstart') {
+      this.seekInProgress = true;
+      $(document.body).addClass('noselect');
+    } else if (!this.seekInProgress) {
+      return;
+    }
+    /* we don't want mouse handlers to receive the event
+     * after touch handlers if we're seeking.
+     */
+    evt.preventDefault();
+    var isTouch = evt.type.slice(0, 5) === 'touch';
+    var pageX = isTouch ? evt.targetTouches.item(0).pageX : evt.pageX;
+    var position = pageX - $progressContainer.offset().left;
+    var containerWidth = $progressContainer.width();
+    var progressRatio = position / containerWidth;
+    this.$progressBar.width((100 * progressRatio) + '%');
+    var duration = this.audio.duration;
+    var targetTime = this.currentTargetTime = progressRatio * duration;
+    this.$timeProgress.text(targetTime.toTime() + ' / ' + duration.toTime());
+  },
+
+  seek: function (e) {
+    $(document.body).removeClass('noselect');
+    if (!this.seekInProgress) {
+      return;
+    }
+    /* we don't want mouse handlers to receive the event
+     * after touch handlers if we're seeking.
+     */
+    e.preventDefault();
+    this.seekInProgress = false;
+    var targetTime = this.currentTargetTime;
+    if (isNaN(targetTime)) {
+      return;
+    }
+    this.audio.currentTime = targetTime;
   }
 
 });
